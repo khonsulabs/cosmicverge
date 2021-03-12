815bd2c9cb45461ebfd39078fd95decb2e372ae2
Jonathan Johnson <jon@khonsulabs.com>
2021-03-12 13:39:09 -0800

    Removed unnecessary cfg(test)

--- database/src/schema/permission_group.rs
+++ database/src/schema/permission_group.rs
@@ -164,34 +164,27 @@ impl Statement {
     }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use crate::test_util::initialize_safe_test;
-    use migrations::pool;
+#[tokio::test]
+async fn create_lookup_delete_test() -> anyhow::Result<()> {
+    crate::test_util::initialize_safe_test().await;
 
-    #[tokio::test]
-    async fn create_lookup_delete() -> anyhow::Result<()> {
-        initialize_safe_test().await;
+    let mut tx = migrations::pool().begin().await?;
+    let group = PermissionGroup::create("create_and_lookup_test", &mut tx).await?;
 
-        let mut tx = pool().begin().await?;
-        let group = PermissionGroup::create("create_and_lookup_test", &mut tx).await?;
+    assert_eq!(
+        group.id,
+        PermissionGroup::find_by_name("create_and_lookup_test", &mut tx)
+            .await?
+            .id
+    );
 
-        assert_eq!(
-            group.id,
-            PermissionGroup::find_by_name("create_and_lookup_test", &mut tx)
-                .await?
-                .id
-        );
+    group.delete(&mut tx).await?;
 
-        group.delete(&mut tx).await?;
+    Ok(())
+}
 
-        Ok(())
-    }
-
-    #[test]
-    fn test_display_permission() {
-        assert_eq!("*", &display_permission(&None));
-        assert_eq!("value", &display_permission(&Some(String::from("value"))));
-    }
+#[test]
+fn display_permission_test() {
+    assert_eq!("*", &display_permission(&None));
+    assert_eq!("value", &display_permission(&Some(String::from("value"))));
 }
