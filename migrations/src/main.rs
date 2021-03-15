87dbbd0fe1dc77e9f6fbaee980907dc1a1fe2020
Jonathan Johnson <jon@khonsulabs.com>
2021-03-15 14:53:23 -0700

    Re-added allow(dead_code) to remove the invalid dead_code warning

--- migrations/src/main.rs
+++ migrations/src/main.rs
@@ -1,4 +1,10 @@
 mod connection;
+
+// cargo warns about code like `migrations::undo_all` not being used when
+// compiling this binary. The warning is incorrect, however, because this module
+// is public when consuming this as a lib. We could mark this mod as pub, but it
+// "feels" more correct to ignore the warning in this binary.
+#[allow(dead_code)]
 mod migrations;
 
 #[tokio::main]
