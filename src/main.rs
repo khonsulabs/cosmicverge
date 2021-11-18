15d156a3668a4f92c6dca5f243e6d5d9699ceda1
Jonathan Johnson <jon@khonsulabs.com>
2021-11-18 08:49:57 -0800

    Adding HSTS; removing bad font

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,34 @@
 use std::path::Path;
 
 use async_trait::async_trait;
-use axum::{error_handling::HandleErrorExt, routing::service_method_routing::get, Router};
-use bonsaidb::server::{
-    AcmeConfiguration, Configuration, DefaultPermissions, Peer, Server, StandardTcpProtocols,
-    TcpService,
+use axum::{
+    error_handling::HandleErrorExt, http::HeaderValue, routing::service_method_routing::get, Router,
 };
-use hyper::{server::conn::Http, StatusCode};
-use tower_http::services::ServeDir;
+use bonsaidb::server::{
+    AcmeConfiguration, Configuration, DefaultPermissions, HttpService, Peer, Server,
+    StandardTcpProtocols,
+};
+use hyper::{header, server::conn::Http, Body, StatusCode};
+use tower_http::{services::ServeDir, set_header::SetResponseHeaderLayer};
 
 #[derive(Debug, Clone)]
 pub struct Website;
 
 #[async_trait]
-impl TcpService for Website {
-    type ApplicationProtocols = StandardTcpProtocols;
-
+impl HttpService for Website {
     async fn handle_connection<
         S: tokio::io::AsyncRead + tokio::io::AsyncWrite + Unpin + Send + 'static,
     >(
         &self,
         connection: S,
-        peer: &Peer<Self::ApplicationProtocols>,
+        peer: &Peer<StandardTcpProtocols>,
     ) -> Result<(), S> {
-        let app = Router::new().nest(
-            "/",
-            get(ServeDir::new("static")).handle_error(|err: std::io::Error| {
-                (
-                    StatusCode::INTERNAL_SERVER_ERROR,
-                    format!("unhandled internal error: {}", err),
-                )
-            }),
-        );
+        let app = router(peer.secure);
+        // Add HSTS header.
+        let app = app.layer(SetResponseHeaderLayer::<_, Body>::if_not_present(
+            header::STRICT_TRANSPORT_SECURITY,
+            HeaderValue::from_static("max-age=31536000; preload"),
+        ));
 
         if let Err(err) = Http::new().serve_connection(connection, app).await {
             log::error!("[http] error serving {}: {:?}", peer.address, err);
@@ -41,11 +38,49 @@ impl TcpService for Website {
     }
 }
 
+fn website() -> Router {
+    Router::new().nest(
+        "/",
+        get(ServeDir::new("static")).handle_error(|err: std::io::Error| {
+            (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                format!("unhandled internal error: {}", err),
+            )
+        }),
+    )
+}
+
+#[cfg(debug_assertions)]
+fn router(_secure: bool) -> Router {
+    website()
+}
+
+#[cfg(not(debug_assertions))]
+fn router(secure: bool) -> Router {
+    if secure {
+        website()
+    } else {
+        Router::new().nest("/", axum::routing::get(redirect_to_https))
+    }
+}
+
+#[cfg(not(debug_assertions))]
+async fn redirect_to_https(req: hyper::Request<Body>) -> hyper::Response<Body> {
+    let path = req.uri().path();
+    let mut response = hyper::Response::new(Body::empty());
+    *response.status_mut() = StatusCode::PERMANENT_REDIRECT;
+    response.headers_mut().insert(
+        "Location",
+        HeaderValue::from_str(&format!("https://cosmicverge.com{}", path)).unwrap(),
+    );
+    response
+}
+
 #[cfg(debug_assertions)]
 const HTTP_LISTEN: &str = ":::8080";
 
 #[cfg(not(debug_assertions))]
-const HTTP_LISTEN: &str = ":::80";
+const HTTP_LISTEN: &str = ":::8080";
 #[cfg(not(debug_assertions))]
 const HTTPS_LISTEN: &str = ":::443";
 
